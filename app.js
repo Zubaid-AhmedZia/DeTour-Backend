19e158f6ec03093c20e5d40f79e4a6fe7bc9f87a
Zubaid-AhmedZia <ziazubaidahmed@gmail.com>
2023-11-06 20:09:39 +0500

    security measures

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
+const reteLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -7,13 +12,48 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
 const app = express();
-//MIDDLEWARES
+//GLOBAL MIDDLEWARES
+//Set security http headers
+app.use(helmet());
+
+//development logging
 console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev')); //logging middleware to genrate http request logs
 }
-app.use(express.json());
 
+//limit requests from same API
+const limiter = reteLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
+
+app.use('/api', limiter);
+
+//body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+//data sanitization against NoSQL query injection e.g {$gt: ""}
+app.use(mongoSanitize());
+
+//data sanitization againXSS cross site scripting
+app.use(xss());
+
+//prevent prameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  }),
+);
+//serving static files
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
